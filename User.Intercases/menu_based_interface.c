78d11c1cbdc54622d3013a7917c0edca006ff335
Glob~Lynn <shoopkayla4@gmail.com>
2024-10-05 17:51:11 +0000

    Final Product

---
+++ User.Intercases/menu_based_interface.c
@@ -0,0 +1,17 @@
+/*This file will hold a basic script for a UI that will display a menu 
+then use input from user to display each option.*/
+
+    //Header Files
+#include <stdio.h>;
+
+    //Begins the Main Function
+int main();
+    //Variables
+
+ switch:
+    case 1:
+    case 2:
+    case 3:
+    default:
+
+
