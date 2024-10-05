1c92b0886d556e5b1e581d8bfbd7157ea85466a4
Glob~Lynn <shoopkayla4@gmail.com>
2024-10-05 18:04:06 +0000

    Final product/Tested

--- User.Intercases/menu_based_interface.c
+++ User.Intercases/menu_based_interface.c
@@ -5,13 +5,41 @@ then use input from user to display each option.*/
 #include <stdio.h>;
 
     //Begins the Main Function
-int main();
+int main(){
     //Variables
+ int UserInput; // This is the option the user will select.
 
- switch:
+ do {
+    printf("TITLE\n"); // Menu starts here
+    printf("\n");
+    printf("1. Option One\n");
+    printf("2. Option Two\n");
+    printf("3. Option Three");
+    printf("\n");
+    printf("Press the NUMBER for which option you want..."); // Request user Input
+    scanf('%d', UserInput); // Storing User input
+
+ switch(UserInput) {
     case 1:
+        printf("You chose Option One...\n");
+     break;
     case 2:
+        printf("You chose Option Two...\n");
+     break;
     case 3:
+        printf("You chose Option Three..\n");
+        printf("Last case closes the menu!\n");
+     break;
     default:
+        if (UserInput < 1 || UserInput > 3){
+         printf("You sure that was A VALID number?..\n");
+         printf("try again!\n");
+        } continue;
+ 
+   }
+ } while (UserInput == 3); {
+    printf("Thanks for playing!\n");
+ }
 
+}// ends the MAIN FUNCTION
 
